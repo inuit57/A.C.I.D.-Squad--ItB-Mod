33eb77199205be5f2a05af63295bd745e2749441
narD <inuit57@naver.com>
2021-02-12 13:19:34 +0900

    이미지 추가 작업

--- scripts/weapons.lua
+++ scripts/weapons.lua
@@ -105,8 +105,13 @@ function narD_PullBeam:GetSkillEffect(p1,p2)
 				
 	end
 	
+	if self.BackACID then
+		local backDamage = SpaceDamage(p1 - DIR_VECTORS[dir] , 0)
+		backDamage.iAcid = 1
+		ret:AddDamage(backDamage) 
+	end
 	
-	--[[ 앞에서부터 끌어당기는 거. 
+	-- 앞에서부터 끌어당기는 거. 
 	-- 문제점 : 맨 앞 놈이 데미지로 죽을 경우 뒤에 충돌피해 없이 그냥 다 땡겨와짐.
 	-- (툴팁에서 보여지는 거랑 달라보일 수가 있음)
 	for i = 1, #targets do
@@ -128,11 +133,11 @@ function narD_PullBeam:GetSkillEffect(p1,p2)
 		temp_dmg = temp_dmg - 1 
 		if temp_dmg < min_dmg then temp_dmg = min_dmg end
 	end
-]]
+
 
 	--맨 뒷놈부터 끌어오는 거. 충돌피해가 생기긴 한다. 
 	-- 문제점 : 맨 앞놈이 죽었을 경우. 두번째 놈만 앞으로 한칸 끌려온다. 막을 수 있을까? 
-	for i = 0, #targets-1  do
+--[[	for i = 0, #targets-1  do
 		local curr = targets[#targets - i]
 		local damage = SpaceDamage(curr, 0, (dir-2)%4)
 		
@@ -159,7 +164,7 @@ function narD_PullBeam:GetSkillEffect(p1,p2)
 		-- temp_dmg = temp_dmg - 1 
 		-- if temp_dmg < min_dmg then temp_dmg = min_dmg end
 	end
-
+]]
 
 	if acid_Bonus  then 
 		local selfDamage = SpaceDamage( p1  ,self.SelfDamage) 
@@ -171,11 +176,7 @@ function narD_PullBeam:GetSkillEffect(p1,p2)
 		ret:AddDamage(selfDamage)
 	end
 
-	if self.BackACID then
-		local backDamage = SpaceDamage(p1 - DIR_VECTORS[dir] , 0)
-		backDamage.iAcid = 1
-		ret:AddDamage(backDamage) 
-	end
+	
 
 	return ret
 end
@@ -289,7 +290,7 @@ narD_VATthrow = ArtilleryDefault:new{-- LineArtillery:new{
 	VatFire = 0,
 	VatPawn = "narD_ACIDVat", 
 	BackACID = false,
-
+	acid_repair = true,
 
 	Acid_Damage = 1,
 	SideACID = 0, 
@@ -343,6 +344,12 @@ function narD_VATthrow:GetSkillEffect(p1,p2)
 		ret:AddDamage(selfDamage)
 	end
 
+	if self.BackACID then
+		local backDamage = SpaceDamage(p1 - DIR_VECTORS[dir] , 0)
+		backDamage.iAcid = 1
+		ret:AddDamage(backDamage) 
+	end
+
 	ret:AddArtillery(damage,"effects/shotup_acid.png")
 	-- shotup_acid
 
@@ -366,11 +373,6 @@ function narD_VATthrow:GetSkillEffect(p1,p2)
 	damagepush.sAnimation = "airpush_"..((dir-1)%4)
 	ret:AddDamage(damagepush)
 	
-	if self.BackACID then
-		local backDamage = SpaceDamage(p1 - DIR_VECTORS[dir] , 0)
-		backDamage.iAcid = 1
-		ret:AddDamage(backDamage) 
-	end
 
 	return ret
 end
